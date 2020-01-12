c5f94712319bcba645565868b20944f02d8beb63
Allen4ever <qx__wang@outlook.com>
2020-01-11 16:18:27 -0800

    Create Assignement_8.5.py

---
+++ Python Data Structures/Assignement_8.5.py
@@ -0,0 +1,11 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for line in fh:
+    line=line.split()
+    if len(line)!=0 and line[0]=='From':
+        print(line[1])
+        count=count+1
+print("There were", count, "lines in the file with From as the first word")
