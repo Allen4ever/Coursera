7940a0ac6764e87f76e76375e64ff7e431746235
Allen4ever <qx__wang@outlook.com>
2020-01-11 17:20:21 -0800

    Update Assignment_10.2.py

--- Python Data Structures/Assignment_10.2.py
+++ Python Data Structures/Assignment_10.2.py
@@ -1,1 +1,11 @@
-
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+counts=dict()
+for line in handle:
+    line=line.split()
+    if len(line)>0 and line[0]=='From':
+        date=line[5].split(':')
+        counts[date[0]]=counts.get(date[0],0)+1
+for k,v in sorted(counts.items()):
+    print(k,v)
