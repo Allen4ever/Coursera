38d58fe39e242decbfbf9e4b2b2b1d060cef98f3
Allen4ever <qx__wang@outlook.com>
2020-01-11 16:51:59 -0800

    Create Assignment_9.4.py

---
+++ Python Data Structures/Assignment_9.4.py
@@ -0,0 +1,14 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+counts=dict()
+for line in handle:
+    line=line.split()
+    if len(line)>0 and line[0]=='From':
+        counts[line[1]]=counts.get(line[1],0)+1
+num_max=0
+for email,num in counts.items():
+    if num>num_max:
+        num_max=num
+        committer=email
+print(committer+'',num_max)
