25338f4554b1f1aceda2df04da05bdafb4c680e3
Allen4ever <qx__wang@outlook.com>
2020-01-12 21:38:43 -0800

    Update and rename Scraping4_Sample_data.py to urllink2.py

---
+++ Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllink2.py
@@ -0,0 +1,25 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+numlist=list()
+for tag in tags:
+    # Look at the parts of a tag
+    numlist.append(int(re.findall('>([0-9]+)<',tag.decode())[0]))
+print(sum(numlist))
