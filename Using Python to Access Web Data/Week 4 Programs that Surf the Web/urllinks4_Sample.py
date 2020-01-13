d05951a4b266d11a7a465c34a8510690d28b2703
Allen4ever <qx__wang@outlook.com>
2020-01-13 00:00:59 -0800

    Update urllinks4_Sample.py

--- Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllinks4_Sample.py
+++ Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllinks4_Sample.py
@@ -1,3 +1,6 @@
+'''
+Following Links in HTML Using BeautifulSoup
+'''
 # To run this, download the BeautifulSoup zip file
 # http://www.py4e.com/code3/bs4.zip
 # and unzip it in the same directory as this file
