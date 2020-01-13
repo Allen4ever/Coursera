daef6470feb3931cce737a9793726a3aed1046a5
Allen4ever <qx__wang@outlook.com>
2020-01-12 22:18:26 -0800

    Update urllink2.py

--- Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllink2.py
+++ Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllink2.py
@@ -12,6 +12,8 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Sample data: http://py4e-data.dr-chuck.net/comments_42.html
+# Actual data: http://py4e-data.dr-chuck.net/comments_355382.html
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
