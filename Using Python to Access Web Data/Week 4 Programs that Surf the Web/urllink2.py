d5a92566ce90ae867c810461fb212d95c2cc0d7e
Allen4ever <qx__wang@outlook.com>
2020-01-12 23:59:29 -0800

    Update urllink2.py

--- Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllink2.py
+++ Using Python to Access Web Data/Week 4 Programs that Surf the Web/urllink2.py
@@ -1,3 +1,6 @@
+'''
+Scraping HTML Data with BeautifulSoup
+'''
 # To run this, download the BeautifulSoup zip file
 # http://www.py4e.com/code3/bs4.zip
 # and unzip it in the same directory as this file
