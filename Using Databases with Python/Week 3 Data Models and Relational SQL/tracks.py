ecc4ad2816050fa7fb7b590b49a739cbd54fdf8f
Allen4ever <qx__wang@outlook.com>
2020-01-17 00:16:42 -0800

    Update tracks.py

--- Using Databases with Python/Week 3 Data Models and Relational SQL/tracks.py
+++ Using Databases with Python/Week 3 Data Models and Relational SQL/tracks.py
@@ -1,6 +1,15 @@
 '''
 Musical Track Database
 '''
+'''
+Database Test:
+    SELECT Track.title, Artist.name, Album.title, Genre.name 
+    FROM Track JOIN Genre JOIN Album JOIN Artist 
+    ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+        AND Album.artist_id = Artist.id
+    ORDER BY Artist.name LIMIT 3
+
+'''
 import xml.etree.ElementTree as ET
 import sqlite3
 
